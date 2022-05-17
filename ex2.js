a40dec5f897418f941c5bf35fd5388d73a2244c5
fataranto <fataranto@gmail.com>
2022-05-17 17:31:58 +0200

    ex2 completo

--- ex2.js
+++ ex2.js
@@ -6,8 +6,8 @@
      return a + b
  }
 
- let sum1; //<-- Solo puedes modificar esta línea
-let sum2; //<-- Solo puedes modificar esta línea
+ let sum1 = 8; //<-- Solo puedes modificar esta línea
+let sum2 = 2;  //<-- Solo puedes modificar esta línea
  let resultado = suma(sum1, sum2)
  console.log(resultado)
 
